83d4303e3b167cfb86603744b2964918269a68a6
malki fruchter <110922997+malkifruchter@users.noreply.github.com>
2022-09-21 00:34:59 +0300

    Time: 2 ms (62.57%), Space: 7.2 MB (92.17%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+       int pairs=0;
+       for(int i=0;i<nums.size();i++)
+       {
+           for(int j=i+1;j<nums.size();j++)
+           {
+               if(nums[j]==nums[i])
+                   pairs++;
+           }
+       }
+        return pairs;
+    }
+};
